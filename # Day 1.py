6892f131b7c1a97008f4a48c5611261c4a6c5f60
Abe Pasion <pasionabe@gmail.com>
2024-07-10 13:23:54 -0400

    Finished Day One!

---
+++ # Day 1.py
@@ -0,0 +1,9 @@
+# Day 1
+# What will we make by the end of the day
+
+print("Welcome to the Band Name Generator.")
+print("What's the name of the city you grew up in?")
+city = input()
+print("What's your pet's name?")
+pet = input()
+print("Your band name could be " + city + " " + pet)
